d37e323adf2380fd753895a948230e390f1eb310
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-01-04 14:21:32 +0900

    Fix: apply renaming

--- src/pages/tags/index.jsx
+++ src/pages/tags/index.jsx
@@ -1,7 +1,7 @@
 import BlogInfoSection from "../../components/sections/BlogInfoSection";
 import TagSection from "../../components/sections/TagSection";
 import Provider from "../../components/atoms/Provider";
-import { getPostTags } from "../../services/postService";
+import { getPostTags } from "../../services/postCollectionService";
 import MainLayout from "../../components/atoms/MainLayout";
 
 export async function getStaticProps() {
