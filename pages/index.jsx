59088d0b906790a22921fcfa7f5781fbd7d3f1f8
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-03-03 16:04:23 +0900

    [SEO] add index page initial seo setting

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 import MainLayout from "../src/@shared/components/Layout";
 import SideBar from "../src/main/components/SideBar";
 import Provider from "../src/@shared/components/Provider";
@@ -6,14 +8,21 @@ import { getPage, getAllPostFileNames } from "../src/@shared/services/post";
 
 function HomePage({ latestPosts, hasNextPage }) {
   return (
-    <MainLayout>
-      <SideBar />
-      <Provider type="vertical"/>
-      <Main
-        posts={latestPosts}
-        hasNextPage={hasNextPage}
-      />
-    </MainLayout>
+    <>
+      <Head>
+        <title>Ponjaehyeok</title>
+        <meta name="description" content="ponjaehyeok blog" />
+        <link rel="shortcut icon" href="/images/profile.PNG" type="image/x-icon" />
+      </Head>
+      <MainLayout>
+        <SideBar />
+        <Provider type="vertical"/>
+        <Main
+          posts={latestPosts}
+          hasNextPage={hasNextPage}
+        />
+      </MainLayout>
+    </>
   );
 }
 
