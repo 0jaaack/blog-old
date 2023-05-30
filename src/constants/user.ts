dce8515e9a6d19137575515d57e32c7903f07c4b
ponjaehyeok <79369983+ponjaehyeok@users.noreply.github.com>
2023-05-30 13:28:06 +0900

    [Fix] fix profile

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -1,7 +1,7 @@
 export const USER = {
   NAME: "ponjaehyeok",
   DESCRIPTION:
-    "최대한 깊게 개념을 탐구해보고\r\n기록하기 위한 블로그\r\n\r\n파고 또 파고..",
+    "최대한 깊게 개념을 탐구해보고\r\n기록하기 위한 블로그\r\n\r\n파고 또 파고..\r\n(라이트 모드는 제작 중입니다)",
   PROFILE_IMAGE_PATH: "/images/profile.PNG",
   GITHUB_URL: "https://github.com/ponjaehyeok",
   EMAIL_URL: "mailto:ruud091@gmail.com",
