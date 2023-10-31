2e19694b25ded7899765948d4c757e37158bfcd0
Jack <jack@hopsoffice.com>
2023-10-31 23:34:46 +0900

    fix constants

--- src/constants/user.ts
+++ src/constants/user.ts
@@ -1,8 +1,8 @@
 export const USER = {
-  NAME: "ponjaehyeok",
+  NAME: "gongjaack",
   DESCRIPTION:
-    "최대한 깊게 개념을 탐구해보고\r\n기록하기 위한 블로그\r\n\r\n파고 또 파고..\r\n(라이트 모드는 제작 중입니다)",
+    "최대한 깊게 개념을 탐구해보고\r\n기록하기 위한 블로그\r\n\r\n파고 또 파고..",
   PROFILE_IMAGE_PATH: "/images/profile.PNG",
-  GITHUB_URL: "https://github.com/ponjaehyeok",
+  GITHUB_URL: "https://github.com/gongjaack",
   EMAIL_URL: "mailto:ruud091@gmail.com",
 };
