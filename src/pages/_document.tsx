500d356663491ab643a5843e9d9cf860326b62c6
Jack <jack@hopsoffice.com>
2023-11-05 02:02:54 +0900

    fix minor

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
-import { DEFAULT_THEME, USER } from "../constants";
+import { USER } from "../constants";
 
 const ThemeInsertionScript = () => {
   const insertTheme = () => {
